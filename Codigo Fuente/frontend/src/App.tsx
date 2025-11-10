7294d40fa14583758043672d487b04d87d27e1f3
TQBlade <144636298+TQBlade@users.noreply.github.com>
2025-11-09 21:37:59 -0500

    first commit

--- Codigo Fuente/frontend/src/App.tsx
+++ Codigo Fuente/frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
 // --- 1. Importa tu página (con el nombre corregido) ---
-import Acceso from './pages/Acceso.jsx'; // 👈 CORREGIDO a singular
+import Acceso from './pages/Acceso.jsx';
 
-// (Aquí puedes importar tus otras páginas, ej: Dashboard)
 // import Dashboard from './pages/Dashboard.jsx'
 
 function App() {
@@ -15,9 +14,9 @@ function App() {
         {/* <Route path="/" element={<Dashboard />} /> */}
 
         {/* 👇 Esta es la ruta que te interesa 👇 */}
-        <Route path="/accesos" element={<Acceso />} /> {/* 👈 CORREGIDO a singular */}
+        <Route path="/accesos" element={<Acceso />} />
 
-        {/* Ruta "comodín" por si no encuentra nada */}
+        {/* Ruta por si no encuentra nada */}
         <Route path="*" element={<h1>404: Página No Encontrada</h1>} />
       </Routes>
     </div>
