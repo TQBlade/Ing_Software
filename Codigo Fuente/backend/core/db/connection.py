b65ca1d9c852ca555d72c0ed8848851a4396b98e
owen854 <145795287+owen854@users.noreply.github.com>
2025-11-10 20:38:52 -0500

    firt commit

--- Codigo Fuente/backend/core/db/connection.py
+++ Codigo Fuente/backend/core/db/connection.py
@@ -1,11 +1,17 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Carga las variables del archivo .env en el entorno
+load_dotenv()
 
 def get_connection():
+    # Llama a las variables de entorno para la conexión
     return psycopg2.connect(
-        host="localhost",
-        database="bd_carros",
-        user="postgres",
-        password="123456",
-        port="5432",
-        options='-c client_encoding=UTF8'
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        client_encoding='UTF8'
     )
